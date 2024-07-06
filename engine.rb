2ff7a01d6977f80cee2131b408ca042e84b3f16c
Gabriel Martinez <gabriel@pixind.com>
2024-07-06 17:17:09 +0200

    Global commands

--- engine.rb
+++ engine.rb
@@ -13,11 +13,9 @@ class TextAdventure
   end
 
   def handle_input(input)
-    if handle_command(input)
-      return get_current_description
-    end
+    return get_current_description if handle_command(input)
 
-    next_state = @data[@current_state]['transitions'][input.downcase]
+    next_state = @data['rooms'][@current_state]['transitions'][input.downcase]
     if next_state
       @current_state = next_state
       get_current_description
@@ -27,29 +25,60 @@ class TextAdventure
   end
 
   def handle_command(input)
-    commands = @data[@current_state]['commands'] || {}
+    commands = @data['global_commands'].merge(@data['rooms'][@current_state]['commands'] || {})
     command_response = commands[input.downcase]
 
     if command_response
-      if input.downcase.start_with?('take')
-        item = input.downcase.split(' ')[1]
-        @inventory << item if command_response.include?("You take the #{item}.")
-      elsif input.downcase.start_with?('attack')
-        # Handle combat logic here
-      end
-
-      puts command_response
+      execute_command(input.downcase, command_response)
+      true
+    elsif input.downcase == 'help'
+      puts 'Available commands: ' + available_commands.join(', ')
       true
     else
       false
     end
   end
 
+  def execute_command(command, response)
+    if command.start_with?('take')
+      item = command.split(' ')[1]
+      @inventory << item if response.include?("You take the #{item}.")
+    elsif command == 'inventory'
+      response = 'You have: ' + @inventory.join(', ')
+    elsif command.start_with?('attack')
+      # Handle combat logic here
+    end
+
+    puts response
+  end
+
   def get_current_description
-    state = @data[@current_state]
+    state = @data['rooms'][@current_state]
     description = state['description']
     description += "\nItems here: " + state['items'].join(', ') if state['items'] && state['items'].any?
-    description += "\nMonsters here: " + state['monsters'].keys.join(', ') if state['monsters'] && state['monsters'].any?
+    if state['monsters'] && state['monsters'].any?
+      description += "\nMonsters here: " + state['monsters'].keys.join(', ')
+    end
+    description += "\nAvailable commands: " + available_commands.join(', ')
     description
   end
+
+  def available_commands
+    global_commands = @data['global_commands'].keys
+    room_commands = @data['rooms'][@current_state]['commands']&.keys || []
+    global_commands + room_commands
+  end
 end
+
+# Usage:
+# require_relative 'text_adventure'
+# adventure = TextAdventure.new('adventure_data.yml')
+# puts adventure.start
+# loop do
+#   print "> "
+#   input = gets.chomp
+#   break if input.downcase == 'exit'
+#   response = adventure.handle_input(input)
+#   puts response
+# end
+# puts "Thanks for playing!"
