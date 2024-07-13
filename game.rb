bc7555b70b476af98dc3de1e631b0cf823fbec53
txarli <txarli@mailbox.org>
2024-07-13 16:38:49 +0200

    Game with muggle and debug mode

--- game.rb
+++ game.rb
@@ -1,4 +1,14 @@
+require 'optparse'
 require_relative 'core/engine'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby game.rb [options]'
+
+  opts.on('--debug', 'Run in debug mode') do
+    options[:debug] = true
+  end
+end.parse!
 
 adventure = Engine.new('./data/game_data.yml')
 
@@ -9,8 +19,18 @@ loop do
   input = gets.chomp
   break if input.downcase == 'q'
 
-  response = adventure.handle_input(input)
-  puts response
+  if options[:debug]
+    response = adventure.handle_input(input)
+    puts response
+  else
+    begin
+      response = adventure.handle_input(input)
+      puts response
+    rescue StandardError => e
+      puts "An error occurred: #{e.message}"
+      puts 'Please try a different command.'
+    end
+  end
 end
 
 puts 'Thanks for playing!'
