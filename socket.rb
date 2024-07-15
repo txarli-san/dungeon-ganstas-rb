a815f80c6bfabea2ede304b276a5f0057bca5f8d
txarli <txarli@mailbox.org>
2024-07-15 20:11:14 +0200

    Splash

--- socket.rb
+++ socket.rb
@@ -8,9 +8,6 @@ EM.run do
   EM::WebSocket.run(host: '0.0.0.0', port: 8080) do |ws|
     ws.onopen do |_handshake|
       puts 'WebSocket connection open'
-      @adventures[ws] = Engine.new('./data/game_data.yml')
-      initial_response = @adventures[ws].start
-      ws.send(GameView.format_response(@adventures[ws], initial_response))
     end
 
     ws.onclose { puts 'Connection closed' }
@@ -18,12 +15,23 @@ EM.run do
     ws.onmessage do |msg|
       puts "Received message: #{msg}"
       msg = msg.strip
-      response = if msg.empty?
-                   'Please enter a command.'
-                 else
-                   @adventures[ws].handle_input(msg)
-                 end
-      ws.send(GameView.format_response(@adventures[ws], response))
+
+      if @adventures[ws].nil?
+        if msg.downcase == 'q'
+          ws.close
+        else
+          @adventures[ws] = Engine.new('./data/game_data.yml')
+          initial_response = @adventures[ws].start
+          ws.send(GameView.format_response(@adventures[ws], initial_response))
+        end
+      else
+        response = if msg.empty?
+                     'Please enter a command.'
+                   else
+                     @adventures[ws].handle_input(msg)
+                   end
+        ws.send(GameView.format_response(@adventures[ws], response))
+      end
     end
   end
 end
